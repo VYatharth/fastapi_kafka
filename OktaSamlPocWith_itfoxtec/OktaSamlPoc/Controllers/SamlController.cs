b524b32fa742b163918ee9cb3b39f2aa0f8ff2ff
yatharthavarshneya <varshneya.yathartha@gmail.com>
2021-05-28 00:20:26 +0530

    Updated Saml controller with new library

--- OktaSamlPocWith_itfoxtec/OktaSamlPoc/Controllers/SamlController.cs
+++ OktaSamlPocWith_itfoxtec/OktaSamlPoc/Controllers/SamlController.cs
@@ -1,14 +1,12 @@
-﻿using ComponentSpace.Saml2;
+﻿using ITfoxtec.Identity.Saml2;
+using ITfoxtec.Identity.Saml2.MvcCore;
+using ITfoxtec.Identity.Saml2.Schemas;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.IdentityModel.Tokens;
-using System;
+using Microsoft.Extensions.Options;
+using Okta_SAML_Example.Identity;
 using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
-using System.Security.Claims;
-using System.Text;
+using System.Security.Authentication;
 using System.Threading.Tasks;
 
 namespace OktaSamlPoc.Controllers
@@ -16,91 +14,136 @@ namespace OktaSamlPoc.Controllers
     [Route("[controller]/[action]")]
     public class SamlController : Controller
     {
-        private readonly ISamlServiceProvider _samlServiceProvider;
-        private readonly IConfiguration _configuration;
-        private const string _logoutCallbackUrl = "/authentication/logout-callback";
+        const string relayStateReturnUrl = "ReturnUrl";
+        private readonly Saml2Configuration config;
+        //private const string _logoutCallbackUrl = "/authentication/logout-callback";
 
-        public SamlController(
-            ISamlServiceProvider samlServiceProvider,
-            IConfiguration configuration)
+        public SamlController(IOptions<Saml2Configuration> configAccessor)
         {
-            _samlServiceProvider = samlServiceProvider;
-            _configuration = configuration;
+            config = configAccessor.Value;
+        }
+        //public SamlController(
+        //    ISamlServiceProvider samlServiceProvider,
+        //    IConfiguration configuration)
+        //{
+        //    _samlServiceProvider = samlServiceProvider;
+        //    _configuration = configuration;
+        //}
+
+        //InitiateSingleSignOn
+        public IActionResult InitiateSingleSignOn(string returnUrl = null)
+        {
+            var binding = new Saml2RedirectBinding();
+            binding.SetRelayStateQuery(new Dictionary<string, string> { { relayStateReturnUrl, returnUrl ?? Url.Content("~/") } });
+
+            return binding.Bind(new Saml2AuthnRequest(config)).ToActionResult();
         }
 
-        public async Task<IActionResult> InitiateSingleSignOn(string returnUrl)
+        //public async Task<IActionResult> InitiateSingleSignOn(string returnUrl)
+        //{
+        //    // To login automatically at the service provider, initiate single sign-on to the identity provider (SP-initiated SSO).            
+        //    var partnerName = _configuration["PartnerName"];
+
+        //    await _samlServiceProvider.InitiateSsoAsync(partnerName, returnUrl);
+
+        //    return new EmptyResult();
+        //}
+
+
+        //[HttpPost("Logout")]
+        //[ValidateAntiForgeryToken]
+        public async Task<IActionResult> InitiateSingleLogout()
         {
-            // To login automatically at the service provider, initiate single sign-on to the identity provider (SP-initiated SSO).            
-            var partnerName = _configuration["PartnerName"];
-
-            await _samlServiceProvider.InitiateSsoAsync(partnerName, returnUrl);
-
-            return new EmptyResult();
-        }
-
-        public async Task<IActionResult> InitiateSingleLogout(string returnUrl)
-        {
-            
-
-            var ssoState = await _samlServiceProvider.GetStatusAsync();
-            
-            if (await ssoState.CanSloAsync())
+            if (!User.Identity.IsAuthenticated)
             {
-               
-                // Request logout at the identity provider.
-                await _samlServiceProvider.InitiateSloAsync();
-
-                return new EmptyResult();
-            }
-            else
-            {
-                ClearSessionData();
-                return Redirect(_logoutCallbackUrl);
+                return Redirect(Url.Content("~/"));
             }
 
+            var binding = new Saml2PostBinding();
+            var saml2LogoutRequest = await new Saml2LogoutRequest(config, User).DeleteSession(HttpContext);
+            return Redirect("~/");
         }
+        //public async Task<IActionResult> InitiateSingleLogout(string returnUrl)
+        //{
+
+
+        //    var ssoState = await _samlServiceProvider.GetStatusAsync();
+
+        //    if (await ssoState.CanSloAsync())
+        //    {
+
+        //        // Request logout at the identity provider.
+        //        await _samlServiceProvider.InitiateSloAsync();
+
+        //        return new EmptyResult();
+        //    }
+        //    else
+        //    {
+        //        ClearSessionData();
+        //        return Redirect(_logoutCallbackUrl);
+        //    }
+
+        //}
 
         public async Task<IActionResult> AssertionConsumerService()
         {
-            // Receive and process the SAML assertion contained in the SAML response.
-            // The SAML response is received either as part of IdP-initiated or SP-initiated SSO.
-            var ssoResult = await _samlServiceProvider.ReceiveSsoAsync();
+            var binding = new Saml2PostBinding();
+            var saml2AuthnResponse = new Saml2AuthnResponse(config);
 
-            // Create and save a JWT to return when requested.
-            var jwtSecurityToken = CreateJwtSecurityToken(ssoResult);
-            HttpContext.Session.SetString("JWT", new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken));
-
-            // Redirect to the specified URL.
-            if (!string.IsNullOrEmpty(ssoResult.RelayState))
+            binding.ReadSamlResponse(Request.ToGenericHttpRequest(), saml2AuthnResponse);
+            if (saml2AuthnResponse.Status != Saml2StatusCodes.Success)
             {
-                return Redirect(ssoResult.RelayState);
+                throw new AuthenticationException($"SAML Response status: {saml2AuthnResponse.Status}");
             }
+            binding.Unbind(Request.ToGenericHttpRequest(), saml2AuthnResponse);
+            await saml2AuthnResponse.CreateSession(HttpContext, claimsTransform: (claimsPrincipal) => ClaimsTransform.Transform(claimsPrincipal));
 
-            return new EmptyResult();
+            var relayStateQuery = binding.GetRelayStateQuery();
+            var returnUrl = relayStateQuery.ContainsKey(relayStateReturnUrl) ? relayStateQuery[relayStateReturnUrl] : Url.Content("~/");
+            return Redirect(returnUrl);
         }
 
-        public async Task<IActionResult> SingleLogoutService()
-        {
+        //public async Task<IActionResult> AssertionConsumerService()
+        //{
+        //    // Receive and process the SAML assertion contained in the SAML response.
+        //    // The SAML response is received either as part of IdP-initiated or SP-initiated SSO.
+        //    var ssoResult = await _samlServiceProvider.ReceiveSsoAsync();
+
+        //    // Create and save a JWT to return when requested.
+        //    var jwtSecurityToken = CreateJwtSecurityToken(ssoResult);
+        //    HttpContext.Session.SetString("JWT", new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken));
+
+        //    // Redirect to the specified URL.
+        //    if (!string.IsNullOrEmpty(ssoResult.RelayState))
+        //    {
+        //        return Redirect(ssoResult.RelayState);
+        //    }
+
+        //    return new EmptyResult();
+        //}
+
+        //public async Task<IActionResult> SingleLogoutService()
+        //{
             
 
-            // Receive the single logout request or response.
-            // If a request is received then single logout is being initiated by the identity provider.
-            // If a response is received then this is in response to single logout having been initiated by the service provider.
-            var sloResult = await _samlServiceProvider.ReceiveSloAsync();
+        //    // Receive the single logout request or response.
+        //    // If a request is received then single logout is being initiated by the identity provider.
+        //    // If a response is received then this is in response to single logout having been initiated by the service provider.
+        //    var sloResult = await _samlServiceProvider.ReceiveSloAsync();
 
-            if (sloResult.IsResponse)
-            {
-                ClearSessionData();
-                return Redirect(_logoutCallbackUrl);
-            }
-            else
-            {
-                // Respond to the IdP-initiated SLO request indicating successful logout.
-                await _samlServiceProvider.SendSloAsync();
-            }
+        //    if (sloResult.IsResponse)
+        //    {
+        //        ClearSessionData();
+        //        return Redirect(_logoutCallbackUrl);
+        //    }
+        //    else
+        //    {
+        //        // Respond to the IdP-initiated SLO request indicating successful logout.
+        //        await _samlServiceProvider.SendSloAsync();
+        //    }
 
-            return new EmptyResult();
-        }
+        //    return new EmptyResult();
+        //}
 
         private void ClearSessionData()
         {
@@ -114,47 +157,47 @@ namespace OktaSamlPoc.Controllers
             
         }
 
-        private JwtSecurityToken CreateJwtSecurityToken(ISpSsoResult ssoResult)
-        {
-            var claims = new List<Claim>
-            {
-                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-                new Claim(JwtRegisteredClaimNames.Sub, ssoResult.UserID)
-            };
+        //private JwtSecurityToken CreateJwtSecurityToken(ISpSsoResult ssoResult)
+        //{
+        //    var claims = new List<Claim>
+        //    {
+        //        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+        //        new Claim(JwtRegisteredClaimNames.Sub, ssoResult.UserID)
+        //    };
 
-            if (ssoResult.Attributes != null)
-            {
-                var samlAttribute = ssoResult.Attributes.SingleOrDefault(a => a.Name == ClaimTypes.Email);
+        //    if (ssoResult.Attributes != null)
+        //    {
+        //        var samlAttribute = ssoResult.Attributes.SingleOrDefault(a => a.Name == ClaimTypes.Email);
 
-                if (samlAttribute != null)
-                {
-                    claims.Add(new Claim(JwtRegisteredClaimNames.Email, samlAttribute.ToString()));
-                }
+        //        if (samlAttribute != null)
+        //        {
+        //            claims.Add(new Claim(JwtRegisteredClaimNames.Email, samlAttribute.ToString()));
+        //        }
 
-                samlAttribute = ssoResult.Attributes.SingleOrDefault(a => a.Name == ClaimTypes.GivenName);
+        //        samlAttribute = ssoResult.Attributes.SingleOrDefault(a => a.Name == ClaimTypes.GivenName);
 
-                if (samlAttribute != null)
-                {
-                    claims.Add(new Claim(JwtRegisteredClaimNames.GivenName, samlAttribute.ToString()));
-                }
+        //        if (samlAttribute != null)
+        //        {
+        //            claims.Add(new Claim(JwtRegisteredClaimNames.GivenName, samlAttribute.ToString()));
+        //        }
 
-                samlAttribute = ssoResult.Attributes.SingleOrDefault(a => a.Name == ClaimTypes.Surname);
+        //        samlAttribute = ssoResult.Attributes.SingleOrDefault(a => a.Name == ClaimTypes.Surname);
 
-                if (samlAttribute != null)
-                {
-                    claims.Add(new Claim(JwtRegisteredClaimNames.FamilyName, samlAttribute.ToString()));
-                }
-            }
+        //        if (samlAttribute != null)
+        //        {
+        //            claims.Add(new Claim(JwtRegisteredClaimNames.FamilyName, samlAttribute.ToString()));
+        //        }
+        //    }
 
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Key"]));
-            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+        //    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Key"]));
+        //    var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
-            return new JwtSecurityToken(
-                _configuration["JWT:Issuer"],
-                _configuration["JWT:Issuer"],
-                claims,
-                expires: DateTime.Now.AddHours(1),
-                signingCredentials: credentials);
-        }
+        //    return new JwtSecurityToken(
+        //        _configuration["JWT:Issuer"],
+        //        _configuration["JWT:Issuer"],
+        //        claims,
+        //        expires: DateTime.Now.AddHours(1),
+        //        signingCredentials: credentials);
+        //}
     }
 }
